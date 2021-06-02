5fffaaae4ff922f5dd5360fe8c5e2ced6989298b
Alexandr Rebikov <rebikovlk@mail.ru>
2021-06-02 17:41:52 +0700

    Повышенный уровень сложности #1

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,11 +1,36 @@
+// Таймер обратного отсчёта
+
 const timerId = document.getElementById('timer');
 
-setInterval(() => {
-    let seconds = +timerId.textContent;
-    if (seconds !== 0) {
-        return timerId.textContent = seconds - 1;
+function timerWin() {
+    return function() {
+        let seconds = +timerId.textContent;
+        if (seconds !== 0) {
+            return timerId.textContent = seconds - 1;
+        }
+        clearInterval(interval);
+        return alert('Вы победили в конкурсе!');
     }
-    return clearInterval();
-}, 100)
+}
 
-setTimeout(() => alert('Вы победили в конкурсе!'), 6000)
+let interval = setInterval(timerWin(), 100)
+
+// Повышенный уровень сложности #1
+
+// const statusId = document.getElementById('status');
+
+// function timer(seconds) {
+//     return function() {
+//         statusId.textContent = `00:00:${seconds}`;
+//         if (+seconds !== 0) {
+//             if (seconds > 10) {
+//                 return seconds -= 1;
+//             } else {
+//                 return seconds = '0' + (seconds - 1);
+//             }
+//         }
+//         clearInterval(interval);
+//         return alert('Вы победили в конкурсе!');
+//     }
+// }
+// let interval = setInterval(timer(30), 100)
