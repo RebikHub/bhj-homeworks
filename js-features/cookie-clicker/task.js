ed97ca61fa8ab8ededfe7b9896b51f2a9dd24d0a
Alexandr Rebikov <rebikovlk@mail.ru>
2021-06-03 13:27:50 +0700

    HTML add div, Clicker add speed-click

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -2,15 +2,19 @@
 
 const cookieId = document.getElementById('cookie');
 const counterId = document.getElementById('clicker__counter');
+const speedClickId = document.getElementById('speed__counter')
+let time = 0;
 
 const clicker = function() {
-    +counterId.textContent;
+    counterId.textContent;
     return function() {
+        speedClickId.textContent = (1 / ((Date.now() - time) / 1000)).toFixed(2)
         if (cookieId.style.width === '200px') {
             cookieId.style.width = '300px';
         } else {
             cookieId.style.width = '200px';
         }
+        time = Date.now();
         return counterId.textContent++;
     }
 }
