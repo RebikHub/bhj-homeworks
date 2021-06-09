0c78e3cabd72e4fe91c73098777574d15ee376e0
Alexandr Rebikov <rebikovlk@mail.ru>
2021-06-09 14:47:30 +0700

    оптимизация по задаче slider

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -2,53 +2,42 @@ const arrayItem = Array.from(document.getElementsByClassName('slider__item'));
 const arrowLeft = document.getElementsByClassName('slider__arrow_prev');
 const arrowRight = document.getElementsByClassName('slider__arrow_next');
 const arrayDots = Array.from(document.getElementsByClassName('slider__dot'));
-let numberImage = 0;
-arrayDots[numberImage].classList.add('slider__dot_active');
+let n = 0;
+
+arrayDots[n].classList.add('slider__dot_active');
+
+function slideImg(arg) {
+    arrayItem.forEach((e, i) => {
+        if (e.classList.contains('slider__item_active')) {
+            arrayDots[i].classList.remove('slider__dot_active');
+            return e.classList.remove('slider__item_active');
+        }
+    })
+    arrayDots[arg].classList.add('slider__dot_active');
+    return arrayItem[arg].classList.add('slider__item_active');
+}
 
 arrowLeft[0].onclick = function() {
-    arrayItem.forEach((e, i) => {
-        if (e.classList.contains('slider__item_active') && i === 0) {
-            e.classList.remove('slider__item_active');
-            arrayDots[i].classList.remove('slider__dot_active');
-            numberImage = arrayItem.length - 1;
-        } else if (e.classList.contains('slider__item_active') && i !== 0) {
-            e.classList.remove('slider__item_active');
-            arrayDots[i].classList.remove('slider__dot_active');
-            numberImage = i - 1;
-        }
-    })
-    arrayDots[numberImage].classList.add('slider__dot_active');
-    return arrayItem[numberImage].classList.add('slider__item_active')
+    if (n === 0) {
+        n = 4;
+        return slideImg(n);
+    } else if (n !== 0) {
+        n -= 1;
+        return slideImg(n);
+    }
 }
-
 arrowRight[0].onclick = function() {
-    arrayItem.forEach((e, i) => {
-        if (e.classList.contains('slider__item_active') && i < 4) {
-            e.classList.remove('slider__item_active');
-            arrayDots[i].classList.remove('slider__dot_active');
-            numberImage = i + 1;
-        } else if (e.classList.contains('slider__item_active') && i === 4) {
-            e.classList.remove('slider__item_active');
-            arrayDots[i].classList.remove('slider__dot_active');
-            numberImage = 0;
-        }
-    })
-    arrayDots[numberImage].classList.add('slider__dot_active');
-    return arrayItem[numberImage].classList.add('slider__item_active')
+    if (n < 4) {
+        n += 1;
+        return slideImg(n);
+    } else if (n === 4) {
+        n = 0;
+        return slideImg(n);
+    }
 }
-
 for (let i = 0; i < arrayDots.length; i++) {
     arrayDots[i].onclick = function() {
-        arrayDots.forEach(element => {
-            if (element.classList.contains('slider__dot_active') && element !== arrayDots[i]) {
-                element.classList.remove('slider__dot_active');
-            }
-        })
-        arrayItem.forEach(element => {
-            if (element.classList.contains('slider__item_active')) {
-                element.classList.remove('slider__item_active');
-            }
-        })
+        slideImg(i);
         arrayDots[i].classList.add('slider__dot_active');
         return arrayItem[i].classList.add('slider__item_active');
     }
