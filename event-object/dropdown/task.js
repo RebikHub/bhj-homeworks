e839b95ba420ce67e4f404341d520fb5c79d58ad
RebikHub <rebikovlk@mail.ru>
2021-06-14 15:35:36 +0700

    Задача 1

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,23 @@
+const dropList = document.querySelector('.dropdown__list');
+const dropValue = document.querySelector('.dropdown__value');
+const dropItem = Array.from(document.getElementsByClassName('dropdown__item'));
+
+function dropClick() {
+    if (dropList.classList.contains('dropdown__list_active')) {
+        dropList.classList.remove('dropdown__list_active');
+    } else {
+        dropList.classList.add('dropdown__list_active');
+    };
+};
+
+function itemClick(event) {
+    event.preventDefault()
+    dropClick();
+    dropValue.textContent = this.textContent;
+};
+
+dropValue.addEventListener('click', dropClick);
+
+for (let item of dropItem) {
+    item.addEventListener('click', itemClick);
+};
