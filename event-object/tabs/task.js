c9120d351d8cbf6d3925867d4799d30f036579fd
Alexandr Rebikov <rebikovlk@mail.ru>
2021-06-15 11:55:12 +0700

    Задача 3

---
+++ event-object/tabs/task.js
@@ -0,0 +1,17 @@
+const arrayTab = Array.from(document.getElementsByClassName('tab'));
+const arrayContent = Array.from(document.getElementsByClassName('tab__content'));
+
+function clickTab() {
+    for (let element of arrayTab) {
+        if (element.classList.contains('tab_active')) {
+            element.classList.remove('tab_active');
+            arrayContent[arrayTab.indexOf(element)].classList.remove('tab__content_active');
+        };
+    };
+    this.classList.add('tab_active');
+    arrayContent[arrayTab.indexOf(this)].classList.add('tab__content_active');
+};
+
+arrayTab.forEach((e) => {
+    e.addEventListener('click', clickTab);
+});
