1bfe3085aa60fdc8a3d1659c49339842c362fe1e
Alexandr Rebikov <rebikovlk@mail.ru>
2021-06-17 14:39:54 +0700

    задача 3+

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,7 +1,40 @@
 const arrFontSize = Array.from(document.getElementsByClassName('font-size'));
+const arrColor = Array.from(document.getElementsByClassName('color'))
 const book = document.querySelector('.book__content');
+const arrTextColor = [];
+const arrBgColor = [];
 
-console.log(arrFontSize);
+arrColor.forEach((e) => {
+    if (e.dataset.textColor) {
+        arrTextColor.push(e);
+    } else if (e.dataset.bgColor) {
+        arrBgColor.push(e);
+    };
+});
+
+function changeTextColor(ev) {
+    ev.preventDefault();
+    arrTextColor.forEach((e) => {
+        if (e.classList.contains('color_active')) {
+            e.classList.remove('color_active');
+            book.classList.remove(`book_color-${e.dataset.textColor}`);
+        };
+    });
+    this.classList.add('color_active');
+    book.classList.add(`book_color-${this.dataset.textColor}`);
+};
+
+function changeBgColor(ev) {
+    ev.preventDefault();
+    arrBgColor.forEach((e) => {
+        if (e.classList.contains('color_active')) {
+            e.classList.remove('color_active');
+            book.classList.remove(`book_bg-${e.dataset.bgColor}`);
+        };
+    });
+    this.classList.add('color_active');
+    book.classList.add(`book_bg-${this.dataset.bgColor}`);
+};
 
 function changeFont(ev) {
     ev.preventDefault();
@@ -10,19 +43,27 @@ function changeFont(ev) {
             e.classList.remove('font-size_active');
             book.classList.remove('font-size_small');
             book.classList.remove('font-size_big');
-        }
-    })
+        };
+    });
     if (this.classList.contains('font-size_small')) {
-        this.classList.add('font-size_active')
+        this.classList.add('font-size_active');
         book.classList.add('font-size_small');
     } else if (this.classList.contains('font-size_big')) {
-        this.classList.add('font-size_active')
+        this.classList.add('font-size_active');
         book.classList.add('font-size_big');
     } else if (!this.classList.contains('font-size_big') && !this.classList.contains('font-size_small')) {
-        this.classList.add('font-size_active')
-    }
-}
+        this.classList.add('font-size_active');
+    };
+};
 
-arrFontSize.forEach((e, i) => {
-    e.addEventListener('click', changeFont)
-})
+arrFontSize.forEach((e) => {
+    e.addEventListener('click', changeFont);
+});
+
+arrTextColor.forEach((e) => {
+    e.addEventListener('click', changeTextColor);
+});
+
+arrBgColor.forEach((e) => {
+    e.addEventListener('click', changeBgColor);
+});
