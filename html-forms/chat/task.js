cfb089884a5535c300d1375903207173c77f3a97
Alexandr Rebikov <rebikovlk@mail.ru>
2021-06-18 14:06:41 +0700

    Задача 1+

--- html-forms/chat/task.js
+++ html-forms/chat/task.js
@@ -7,11 +7,12 @@ const answers = [
     'Добрый день, мы ещё не проснулись. Позвоните через 10 лет',
     'Мы ничего не будем вам продавать!',
     'До свидания!'
-]
-
-widget.addEventListener('click', () => widget.classList.add('chat-widget_active'));
+];
+let timerId;
 
 function inputMessage(e) {
+    stopTimer();
+    startTimer();
     let index = Math.floor(Math.random() * (answers.length));
     if (e.key === 'Enter' && input.value !== '') {
         messages.innerHTML += `
@@ -25,7 +26,29 @@ function inputMessage(e) {
             <div class="message__time">${`${(new Date).getHours()}:${(new Date).getMinutes()}`}</div>
             <div class="message__text">${answers[index]}</div>
         </div>`
-    }
+    };
+    messages.scrollIntoView(false);
 };
+
+function question() {
+    if (widget.classList.contains('chat-widget_active')) {
+        messages.innerHTML += `
+        <div class="message">
+            <div class="message__time">${`${(new Date).getHours()}:${(new Date).getMinutes()}`}</div>
+            <div class="message__text">Вы еще здесь?</div>
+        </div>`
+    };
+    messages.scrollIntoView(false);
+};
+
+function startTimer() {
+    return timerId = setTimeout(question, 30000);
+};
+
+function stopTimer() {
+    return timerId = clearTimeout(timerId);
+};
+
+widget.addEventListener('click', () => widget.classList.add('chat-widget_active'));
 
 input.addEventListener('keyup', inputMessage);
