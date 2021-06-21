f98ae7c694b27bc5e60423d825af6ddca446da53
Alexandr Rebikov <rebikovlk@mail.ru>
2021-06-21 16:58:23 +0700

    checkboxes+

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,25 +1,33 @@
 const checkbox = document.getElementsByClassName('interest__check');
+const ul = document.querySelectorAll('ul');
+
+
+console.log(checkbox);
+console.log(ul)
+
 
 function onChecked(e) {
-    if (e.target.checked !== true) {
-        e.target.checked = false;
-        for (let index of checkbox) {
-            if (index.checked === true) {
-                index.checked = false;
-            };
-        };
-    } else {
-        for (let index of checkbox) {
-            if (index.checked === true) {
-                index.checked = false;
-            };
-        };
-        let parent = e.target.closest('.interest');
-        for (let index of parent.querySelectorAll('input')) {
+    let parents = e.target.closest('.interests').parentElement.querySelector('input')
+    let childs = e.target.closest('.interest').querySelectorAll('input');
+    console.log(this.checked);
+
+    if (this.checked === true) {
+        for (let index of childs) {
             index.checked = true;
         };
+    } else {
+        for (let index of childs) {
+            index.checked = false;
+        };
     }
-};
+}
+// if (e.target.closest('.interests').parentElement) {
+//     console.log('yes');
+// }
+// console.log(parents);
+// parents.indeterminate = true
+// }
+
 
 for (let element of checkbox) {
     element.addEventListener('change', onChecked);
