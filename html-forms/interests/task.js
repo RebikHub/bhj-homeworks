2fc266e821b211fa744ce73837ca168c888634b6
Alexandr Rebikov <rebikovlk@mail.ru>
2021-06-23 11:53:37 +0700

    Задача 2+ version from Jakob Eriksen

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,82 +1,48 @@
 const checkbox = Array.from(document.getElementsByClassName('interest__check'));
 
-function nodeArray(selector, parent = document) {
-    return [].slice.call(parent.querySelectorAll(selector));
-};
-
 function onCheckedParents(e) {
     let check = e.target;
 
     if (!checkbox.indexOf(check)) {
         return;
-    };
-    const children = nodeArray('input', check.parentNode);
-    children.forEach(child => child.checked = check.checked);
+    } else {
+        while (check) {
+            const parent = check.closest('ul').parentElement.querySelector('input');
+            const sibling = Array.from((parent.closest('li').querySelector('ul')).querySelectorAll('input'));
+            const checkStatus = sibling.map(elem => elem.checked === true);
+            const every = checkStatus.every(elem => elem === true);
+            const some = checkStatus.some(elem => elem === true);
+            parent.checked = every;
 
-    while (check) {
-        const parent = check.closest('ul').parentElement.querySelector('input');
-        const sibling = nodeArray('input', parent.closest('li').querySelector('ul'));
-        const checkStatus = sibling.map(elem => elem.checked);
-        const every = checkStatus.every(Boolean);
-        const some = checkStatus.some(Boolean);
-        parent.checked = every;
+            if (!every && every !== some) {
+                parent.indeterminate = true;
+            } else {
+                parent.indeterminate = false;
+            };
 
-        if (!every && every !== some) {
-            parent.indeterminate = true;
-        } else {
-            parent.indeterminate = false;
+            if (check != parent) {
+                check = parent;
+            } else {
+                check = 0;
+            };
         };
-
-        if (check != parent) {
-            check = parent;
-        } else {
-            check = 0;
-        };
-    };
+    }
 }
 
 function onCheckedChildren(e) {
-    let childs = e.target.closest('.interest').querySelectorAll('input');
+    const children = e.target.closest('.interest').querySelectorAll('input');
     if (this.checked === true) {
-        for (let index of childs) {
+        for (let index of children) {
             index.checked = true;
         };
     } else {
-        for (let index of childs) {
+        for (let index of children) {
             index.checked = false;
         };
     }
 }
 
-for (let element of checkbox) {
-    element.addEventListener('change', onCheckedChildren);
-    element.addEventListener('change', onCheckedParents);
+for (let index of checkbox) {
+    index.addEventListener('change', onCheckedChildren);
+    index.addEventListener('change', onCheckedParents);
 };
-
-
-// function onCheckedParents(e) {
-//     let parents = e.target.closest('ul').parentElement.querySelector('input');
-//     let sibling = Array.from(e.target.closest('ul').querySelectorAll('input'));
-
-//     if (e.target.matches('.interests_active .interest__check')) {
-//         parents.closest('ul').parentElement.querySelector('input').indeterminate = true
-//         parents.indeterminate = true
-//     } else {
-//         parents.closest('ul').parentElement.querySelector('input').indeterminate = false
-//         parents.indeterminate = false
-//     }
-
-//     for (let element of checkbox) {
-//         if (element.checked === true && element.closest('ul').parentElement.querySelector('input').indeterminate === false) {
-//             element.closest('ul').parentElement.querySelector('input').indeterminate = true
-//         }
-//     }
-//     for (let i = 0; i < checkbox.length; i++) {
-//         if (checkbox[i].closest('.interest').querySelectorAll('input').length > 1) {}
-//         for (const el of checkbox[i].closest('.interest').querySelectorAll('input')) {
-//             if (el.checked === true) {
-//                 checkbox[i].closest('ul').parentElement.querySelector('input').indeterminate = true
-//             }
-//         }
-//     }
-// }
