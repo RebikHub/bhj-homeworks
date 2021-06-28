2e46f5d080e1a8ee048daad19752e11bbcf59e06
Alexandr Rebikov <rebikovlk@mail.ru>
2021-06-28 10:27:59 +0700

    Задача 1 доработка

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -2,22 +2,22 @@ const hasTooltip = document.getElementsByClassName('has-tooltip');
 const tooltip = document.querySelector('.tooltip');
 tooltip.setAttribute('data-position', 'bottom');
 
-function removeClass() {
-    if (tooltip.classList.contains('tooltip_active')) {
-        return tooltip.classList.remove('tooltip_active');
-    };
-};
 
 function onClick(e) {
     e.preventDefault();
-    removeClass();
+
     let left = e.target.getBoundingClientRect().left;
     let top = e.target.getBoundingClientRect().top;
-    tooltip.classList.add('tooltip_active');
+
+    if (tooltip.classList.contains('tooltip_active') && tooltip.textContent === e.target.title) {
+        tooltip.classList.remove('tooltip_active');
+    } else {
+        tooltip.classList.add('tooltip_active');
+    };
+
     tooltip.style.left = `${left}px`;
     tooltip.style.top = `${top + 20}px`;
     tooltip.textContent = e.target.title;
-    setTimeout(removeClass, 2000);
 };
 
 for (let index of hasTooltip) {
