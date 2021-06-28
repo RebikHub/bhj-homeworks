1ce0290ed5b3da1a06e2c000428de8356debea8e
RebikHub <rebikovlk@mail.ru>
2021-06-28 20:26:29 +0700

    Доработка 2ой задачи

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,11 +1,10 @@
 const input = document.getElementById('task__input');
 const tasksList = document.getElementById('tasks__list');
 const tasksAdd = document.getElementById('tasks__add');
+let taskRemove = document.getElementsByClassName('task__remove');
 let task;
 let taskTitle;
-let taskRemove = document.getElementsByClassName('task__remove');
 let todo = '';
-let todoSave = [];
 
 createElement();
 getTasks();
@@ -19,10 +18,10 @@ function createElement() {
     let arrayTask = [];
     for (let i = 1; i < 6; i++) {
         arrayTask.push(html);
-    }
-    tasksList.insertAdjacentHTML('beforeend', arrayTask.join(''))
+    };
+    tasksList.insertAdjacentHTML('beforeend', arrayTask.join(''));
     task = document.getElementsByClassName('task');
-}
+};
 
 function onInput(e) {
     todo = e.target.value;
@@ -38,7 +37,7 @@ function buttonAdd() {
     taskTitle = Array.from(document.getElementsByClassName('task__title'));
     let index = taskTitle.findIndex(e => e.textContent === '');
     if (todo.trim() !== '') {
-        if (index >= 0) {
+        if ((taskTitle.length - 1) > index && index >= 0) {
             taskTitle[index].innerText = todo;
             taskTitle[index].closest('.task').style = null;
             removeToDo();
@@ -48,39 +47,41 @@ function buttonAdd() {
             taskTitle[index].innerText = todo;
             taskTitle[index].closest('.task').style = null;
             removeToDo();
+            saveTasks();
         }
-        input.value = '';
-    };
+    }
+    input.value = '';
 };
 
 function saveTasks() {
-    todoSave.push(todo);
+    let todoSave = [];
+    taskTitle = Array.from(document.getElementsByClassName('task__title'));
+    for (let i = 0; i < taskTitle.length; i++) {
+        if (taskTitle[i].textContent !== '') {
+            todoSave.push(taskTitle[i].textContent);
+        };
+    };
     localStorage.task = todoSave;
-    console.log(todoSave);
 };
 
 function getTasks() {
     if (localStorage.task) {
-        todoSave = localStorage.task.split(',');
+        let todoSave = localStorage.task.split(',');
         let arrayTask = [];
         taskTitle = Array.from(document.getElementsByClassName('task__title'));
         for (let i = 0; i < todoSave.length; i++) {
-
-            arrayTask.push(task[0].cloneNode(true))
+            arrayTask.push(task[0].cloneNode(true));
         }
         arrayTask.forEach((e, i) => {
             e.querySelector('.task__title').textContent = `${todoSave[i]}`;
             e.style = null;
-            return tasksList.appendChild(e)
+            return tasksList.appendChild(e);
         });
         removeToDo();
-    }
-}
+    };
+};
 
 function removeToDo() {
-    taskTitle = Array.from(document.getElementsByClassName('task__title'));
-    todoSave = taskTitle.map(e => e.textContent);
-    console.log(todoSave);
     taskRemove = document.getElementsByClassName('task__remove');
     for (let index of taskRemove) {
         index.addEventListener('click', () => {
