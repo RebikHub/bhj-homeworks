fede6ba558d0d7b4213e4df37ba944cd2ef2a11e
Alexandr Rebikov <rebikovlk@mail.ru>
2021-06-29 12:36:17 +0700

    Задача 1+

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -1,8 +1,11 @@
 let xhr = new XMLHttpRequest();
 let excRates = {};
-const item = document.querySelector('.item');
+let item = document.querySelector('.item');
 const imgLoader = document.querySelector('#loader');
 const items = document.querySelector('#items');
+let itemCode = document.querySelectorAll('.item__code');
+let itemValue = document.querySelectorAll('.item__value');
+let itemCurrency = document.querySelectorAll('.item__currency');
 
 function getExcRates() {
     xhr.open('GET', 'https://netology-slow-rest.herokuapp.com');
@@ -21,23 +24,22 @@ function getExcRates() {
 };
 
 function insertElement() {
+    let arrayClone = [];
     for (let i = 0; i < 3; i++) {
-        let clone = item.cloneNode(true);
-        items.insertAdjacentElement('afterbegin', clone);
-    };
-    let itemHtml = `<div class="item__code"></div>
-    <div class="item__value"></div>
-    <div class="item__currency"></div>`;
-    for (let i = 0; i < document.querySelectorAll('.item').length; i++) {
-        document.querySelectorAll('.item')[i].insertAdjacentHTML('afterbegin', itemHtml);
-    };
+        arrayClone.push(`<div class="item">
+            <div class="item__code"></div>
+            <div class="item__value"></div>
+            <div class="item__currency"></div>
+        </div>`);
+    }
+    items.insertAdjacentHTML('afterbegin', arrayClone.join(''));
     getExcRates();
 };
 
 function getValueRates() {
-    const itemCode = document.querySelectorAll('.item__code');
-    const itemValue = document.querySelectorAll('.item__value');
-    const itemCurrency = document.querySelectorAll('.item__currency');
+    itemCode = document.querySelectorAll('.item__code');
+    itemValue = document.querySelectorAll('.item__value');
+    itemCurrency = document.querySelectorAll('.item__currency');
 
     itemCode[0].textContent = excRates.USD.CharCode;
     itemValue[0].textContent = excRates.USD.Value;
@@ -52,18 +54,70 @@ function getValueRates() {
 };
 
 function saveValute() {
-    let html = items.innerHTML;
-    localStorage.setItem('usd', html);
+    let item = document.getElementsByClassName('item');
+    itemCode = document.querySelectorAll('.item__code');
+    itemValue = document.querySelectorAll('.item__value');
+    itemCurrency = document.querySelectorAll('.item__currency');
+    let usd = {};
+    let eur = {};
+    let gbp = {};
+    for (let i = 0; i < item.length; i++) {
+        if (item[i].querySelector('.item__code').textContent === 'USD') {
+
+            usd.code = itemCode[i].textContent;
+            usd.value = itemValue[i].textContent;
+            usd.currency = 'руб.';
+        } else if (item[i].querySelector('.item__code').textContent === 'EUR') {
+            eur.code = itemCode[i].textContent;
+            eur.value = itemValue[i].textContent;
+            eur.currency = 'руб.';
+        } else if (item[i].querySelector('.item__code').textContent === 'GBP') {
+            gbp.code = itemCode[i].textContent;
+            gbp.value = itemValue[i].textContent;
+            gbp.currency = 'руб.';
+        };
+    };
+    localStorage.usd = JSON.stringify(usd);
+    localStorage.eur = JSON.stringify(eur);
+    localStorage.gbp = JSON.stringify(gbp);
 };
 
 function getValute() {
-    if (localStorage.getItem('usd')) {
-        getExcRates();
-        return items.innerHTML = localStorage.getItem('usd');
+    let usd;
+    let eur;
+    let gbp;
+    let arrayValute;
+    if (localStorage.length !== 0) {
+        usd = JSON.parse(localStorage.usd);
+        eur = JSON.parse(localStorage.eur);
+        gbp = JSON.parse(localStorage.gbp);
+        arrayValute = [];
     } else {
-        items.innerHTML = '';
         return insertElement();
     };
+    if (Object.keys(usd).length !== 0) {
+        arrayValute.push(`<div class="item">
+        <div class="item__code">${usd.code}</div>
+        <div class="item__value">${usd.value}</div>
+        <div class="item__currency">руб.</div>
+    </div>`);
+    };
+    if (Object.keys(eur).length !== 0) {
+        arrayValute.push(`<div class="item">
+        <div class="item__code">${eur.code}</div>
+        <div class="item__value">${eur.value}</div>
+        <div class="item__currency">руб.</div>
+    </div>`);
+    };
+    if (Object.keys(gbp).length !== 0) {
+        arrayValute.push(`<div class="item">
+        <div class="item__code">${gbp.code}</div>
+        <div class="item__value">${gbp.value}</div>
+        <div class="item__currency">руб.</div>
+    </div>`);
+    };
+    getExcRates();
+    return items.insertAdjacentHTML('afterbegin', arrayValute.join(''));
 };
 
 getValute();
