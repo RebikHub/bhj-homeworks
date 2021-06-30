71a1949dc443dd924bd391f09fe01e395fcdaf46
Alexandr Rebikov <rebikovlk@mail.ru>
2021-06-30 13:02:24 +0700

    Задача 1

---
+++ client-state/text-editor/task.js
@@ -0,0 +1,21 @@
+const editor = document.querySelector('#editor');
+const btnClear = document.querySelector('.clear-textarea');
+
+pasteText();
+
+function onEditor() {
+    localStorage.text = JSON.stringify(editor.value);
+}
+
+function onClear() {
+    editor.value = '';
+}
+
+function pasteText() {
+    if (localStorage.text) {
+        editor.value = JSON.parse(localStorage.text);
+    }
+}
+
+editor.addEventListener('input', onEditor);
+btnClear.addEventListener('click', onClear)
