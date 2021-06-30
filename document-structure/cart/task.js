737df54115e1c8bd87d595f8039cbcf08918cd4f
Alexandr Rebikov <rebikovlk@mail.ru>
2021-06-30 12:02:00 +0700

    fix localStorage

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -86,75 +86,35 @@ function stopTimer() {
 
 function saveBascet() {
     let arrayIds = document.getElementsByClassName('cart__product');
-    let caviar = {};
-    let cook = {};
-    let pike = {};
-    let crab = {};
+    let arrayCart = [];
 
     for (let i = 0; i < arrayIds.length; i++) {
-        if (+arrayIds[i].dataset.id === 1) {
-            caviar.id = arrayIds[i].dataset.id;
-            caviar.img = arrayIds[i].querySelector('.cart__product-image').src;
-            caviar.count = arrayIds[i].querySelector('.cart__product-count').textContent;
-        } else if (+arrayIds[i].dataset.id === 2) {
-            cook.id = arrayIds[i].dataset.id;
-            cook.img = arrayIds[i].querySelector('.cart__product-image').src;
-            cook.count = arrayIds[i].querySelector('.cart__product-count').textContent;
-        } else if (+arrayIds[i].dataset.id === 3) {
-            pike.id = arrayIds[i].dataset.id;
-            pike.img = arrayIds[i].querySelector('.cart__product-image').src;
-            pike.count = arrayIds[i].querySelector('.cart__product-count').textContent;
-        } else {
-            crab.id = arrayIds[i].dataset.id;
-            crab.img = arrayIds[i].querySelector('.cart__product-image').src;
-            crab.count = arrayIds[i].querySelector('.cart__product-count').textContent;
-        };
+        arrayCart.push({
+            id: arrayIds[i].dataset.id,
+            img: arrayIds[i].querySelector('.cart__product-image').src,
+            count: arrayIds[i].querySelector('.cart__product-count').textContent
+        });
     };
-    localStorage.caviar = JSON.stringify(caviar);
-    localStorage.cook = JSON.stringify(cook);
-    localStorage.pike = JSON.stringify(pike);
-    localStorage.crab = JSON.stringify(crab);
+
+    localStorage.cart = JSON.stringify(arrayCart);
 };
 
 function getBascet() {
-    let caviar;
-    let cook;
-    let pike;
-    let crab;
     let arrayBascet;
-    if (localStorage.length !== 0) {
-        caviar = JSON.parse(localStorage.caviar);
-        cook = JSON.parse(localStorage.cook);
-        pike = JSON.parse(localStorage.pike);
-        crab = JSON.parse(localStorage.crab);
+    let arrayStorage;
+
+    if (localStorage.cart) {
+        arrayStorage = JSON.parse(localStorage.cart);
         arrayBascet = [];
     } else {
         return;
     };
 
-    if (Object.keys(caviar).length !== 0) {
-        arrayBascet.push(`<div class="cart__product" data-id="${caviar.id}">
-    <img class="cart__product-image" src="${caviar.img}">
-    <div class="cart__product-count">${caviar.count}</div>
+    for (let i = 0; i < arrayStorage.length; i++) {
+        arrayBascet.push(`<div class="cart__product" data-id="${arrayStorage[i].id}">
+    <img class="cart__product-image" src="${arrayStorage[i].img}">
+    <div class="cart__product-count">${arrayStorage[i].count}</div>
 </div>`);
-    };
-    if (Object.keys(cook).length !== 0) {
-        arrayBascet.push(`<div class="cart__product" data-id="${cook.id}">
-        <img class="cart__product-image" src="${cook.img}">
-        <div class="cart__product-count">${cook.count}</div>
-    </div>`);
-    };
-    if (Object.keys(pike).length !== 0) {
-        arrayBascet.push(`<div class="cart__product" data-id="${pike.id}">
-        <img class="cart__product-image" src="${pike.img}">
-        <div class="cart__product-count">${pike.count}</div>
-    </div>`);
-    };
-    if (Object.keys(crab).length !== 0) {
-        arrayBascet.push(`<div class="cart__product" data-id="${crab.id}">
-        <img class="cart__product-image" src="${crab.img}">
-        <div class="cart__product-count">${crab.count}</div>
-    </div>`);
     };
     cart.style = 'display: block';
     cartProducts.insertAdjacentHTML('afterbegin', arrayBascet.join(''));
